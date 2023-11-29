c04f336a77c62a090290744de39cba6f585d39a7
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-29 21:21:06 +0700

    ganti tampilan

--- README.md
+++
@@ -1,1 +0,0 @@
-# dashboard Human Resource Monitoring
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-   plugins: {
-      tailwindcss: { config: "./tailwindcss-config.js" },
-      autoprefixer: {},
-   },
-};
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ],
+}
